cd82d717bf3849202f229aff7c54eb1a7585d6f1
MadhuKumarGowda <madhukumar.gowda@gmail.com>
2024-03-08 00:41:41 +0530

    Front end app set up - install bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import {  HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, HttpClientModule, ReactiveFormsModule, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
